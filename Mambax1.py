5dbf83e91c084ff7cbd30eb9193b1dc582da39c8
techybanana <73810850+techybanana@users.noreply.github.com>
2020-11-15 18:56:38 -0800

    Add files via upload

---
+++ Mambax1.py
@@ -0,0 +1,101 @@
+import time, random, webbrowser
+print('Welcome to MambaX1 Launcher. Type # to start.')
+for i in range(200000000000):
+    startlogin = str(input('@ '))
+    if startlogin == '#':
+        print('Please login...')
+        time.sleep(1)
+        startuser = str(input('Username: '))
+        if startuser == 'trial':
+            time.sleep(2)
+            startpass = str(input('Password: '))
+            if startpass == 'trial':
+                time.sleep(1)
+                for i in range(238893723974):
+                    com = str(input('$$: '))
+                    if com == '@info':
+                        print('Mamba is by Viiper Inc. It is made by its CEO, Naitik.\n Go to naitikpgupta.wixsite.com/mamba for more info.\n Mamba ID: TRIAL:JUSTFORSHOWCASE\n When yiu type to download something (e.g. mamba get ws -jet) it only downloads the .exe of it.\n Then you have to download it.\n In MambaX2, coming 11/30/2020, it opens the exe for you. To check for beta or omega releases, type mamba checkX2')
+                    if com == 'mamba get pycharm':
+                        webbrowser.open('https://download-cf.jetbrains.com/python/pycharm-community-2020.2.3.exe')
+
+                    if com == 'mamba get mps':
+                        webbrowser.open('https://download-cf.jetbrains.com/mps/2020.2/MPS-2020.2.2.exe')
+
+                    if com == 'mamba get inj -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/idea/ideaIC-2020.2.3.exe')
+
+                    if com == 'mamba get teamc -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/idea/ideaIC-2020.2.3.exe')
+
+                    if com == 'mamba get clion':
+                        webbrowser.open('https://download-cf.jetbrains.com/cpp/CLion-2020.2.4.exe')
+
+                    if com == 'mamba get dg -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/datagrip/datagrip-2020.2.3.exe')
+
+                    if com == 'mamba get gl -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/go/goland-2020.2.3.exe')
+
+                    if com == 'mamba get phps -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/webide/PhpStorm-2020.2.3.exe')
+
+                    if com == 'mamba get rider':
+                        webbrowser.open('https://download-cf.jetbrains.com/rider/JetBrains.Rider-2020.2.4.exe')
+
+                    if com == 'mamba get rubm -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/ruby/RubyMine-2020.2.3.exe')
+
+                    if com == 'mamba get ws -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/webstorm/WebStorm-2020.2.3.exe')
+
+                    if com == 'mamba get rs -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/resharper/dotUltimate.2020.2.4/JetBrains.dotUltimate.2020.2.4.exe')
+
+                    if com == 'mamba get vs.plugins -jet':
+                        webbrowser.open('https://download-cf.jetbrains.com/resharper/dotUltimate.2020.2.4/JetBrains.dotTrace.2020.2.4.web.exe')
+
+                        webbrowser.open('https://download-cf.jetbrains.com/resharper/dotUltimate.2020.2.4/JetBrains.dotPeek.2020.2.4.web.exe')
+
+                        webbrowser.open('https://download-cf.jetbrains.com/resharper/dotUltimate.2020.2.4/JetBrains.dotMemory.2020.2.4.web.exe')
+
+                        webbrowser.open('https://download-cf.jetbrains.com/resharper/dotUltimate.2020.2.4/JetBrains.dotCover.2020.2.4.web.exe')
+
+                        webbrowser.open('https://download-cf.jetbrains.com/resharper/dotUltimate.2020.2.4/JetBrains.ReSharperCpp.2020.2.4.web.exe')
+
+                        webbrowser.open('https://download-cf.jetbrains.com/resharper/dotUltimate.2020.2.4/JetBrains.dotUltimate.2020.2.4.exe')
+
+                    if com == 'mamba checkX2':
+                        print('Checking...')
+                        time.sleep(2)
+                        print('No realses yet. Sorry about that!')
+                    if com == 'mamba get .NET':
+                        print('Checking Latest Version...')
+                        time.sleep(3)
+                        print('Found!!!')
+                        time.sleep(2)
+                        webbrowser.open('https://dotnet.microsoft.com/download/dotnet/thank-you/sdk-5.0.100-windows-x64-installer')
+
+                        webbrowser.open('https://dotnet.microsoft.com/download/dotnet/thank-you/runtime-desktop-5.0.0-windows-x64-installer')
+
+                    if com == 'mamba get chrome':
+                        webbrowser.open('https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B02D446EA-A91E-4630-473C-D78DAD5A1334%7D%26lang%3Den%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe')
+
+                    if com == 'mamba get obs':
+                        webbrowser.open('https://cdn-fastly.obsproject.com/downloads/OBS-Studio-26.0.2-Full-Installer-x64.exe')
+
+                    if com == 'mamba get py.3':
+                        webbrowser.open('https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe')
+                    if com == 'mamba get visualstudio.com':
+                        webbrowser.open('https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&rel=16')
+                    if com == 'mamba get vsc.com':
+                        webbrowser.open('https://code.visualstudio.com/docs/?dv=win')
+                    if com == 'mamba hub':
+                        webbrowser.open('https://github.com/viiperincsoftware/mamax1')
+                    if com == 'mamba get X2':
+                        webbrowser.open('https://github.com/viiperincsoftware/mambax2')
+            else:
+                print('Wrong. Pls restart.')
+        else:
+            print('Wrong. Pls restart.')
+    else:
+        print('Pls type #')
---
+++ Readme.txt.txt
@@ -0,0 +1,6 @@
+Hello,
+ This is Mamba X1. In Mamba X1 you just have to type a line, then you can download a wide varity of software.
+	To sign in go to the auth text document. License is in lic text document. Commands is in com.txt
+	
+Best of wishes,
+	Mamba Team from Viiper Inc.
